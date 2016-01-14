f14c3350f6d3f5c5470ff0c46d0baa09a312d2ba
Ingram Monk <ingram.monk@gmail.com>
2016-01-14 12:26:23 +0000

    finished data feed download filesystem fallback

--- src/DataFeedDownloader.php
+++ src/DataFeedDownloader.php
@@ -2,24 +2,32 @@
 
 namespace IFP\Adverts;
 
+use Bugsnag_Client;
+
 class DataFeedDownloader
 {
     private $url;
     private $curl;
     private $data;
     private $downloaded_file_save_location;
+    private $bugsnag_client;
 
-    public function __construct(Curl $curl, $url, $downloaded_file_save_location)
+    public function __construct(Curl $curl, $url, $downloaded_file_save_location, $bugsnag_api_key = null)
     {
         $this->curl = $curl;
         $this->curl->init($url);
         $this->url = $url;
         $this->downloaded_file_save_location = $downloaded_file_save_location;
+        $this->loadBugsnag($bugsnag_api_key);
     }
 
     public function data()
     {
-        $this->data = $this->download();
+        try {
+            $this->data = $this->download();
+        } catch (UnableToDownloadDataException $e) {
+            $this->data = $this->readPreviouslyDownloadedFile();
+        }
 
         return $this->data;
     }
@@ -39,26 +47,57 @@ class DataFeedDownloader
             throw new UnableToDownloadDataException('Unable to download data feed ' . $this->url . ', see exception code for http status code. Last php error message: ' . error_get_last()['message'], $http_code);
         }
 
+        $this->saveDownloadedFile($data);
+
         return $data;
     }
 
-    public function saveDownloadedFile($data)
+    private function saveDownloadedFile($data)
     {
         if (false === @file_put_contents($this->downloaded_file_save_location, $data)) {
-            throw new UnableToWriteDataToDiskException('Failure while storing downloaded file : ' . error_get_last()['message']);
+            $e = new UnableToWriteDataToDiskException('Failure while storing downloaded file : ' . error_get_last()['message']);
+            $this->bugsnagNotifyException($e);
+            throw $e;
         }
 
         return true;
     }
 
-    public function readPreviouslyDownloadedFile()
+    private function readPreviouslyDownloadedFile()
     {
         $data = @file_get_contents($this->downloaded_file_save_location);
 
         if ($data === false) {
-            throw new UnableToReadFileFromDiskException('Failure while reading downloaded file : ' . error_get_last()['message']);
+            $e = new UnableToReadFileFromDiskException('Failure while reading downloaded file : ' . error_get_last()['message']);
+            $this->bugsnagNotifyException($e);
+            throw $e;
+        } else {
+            $this->bugsnagNotifyError('DataFileLoadedFromDisk', 'Data file not downloaded from ' . $this->url . ', but instead read from filesystem ' . $this->downloaded_file_save_location);
         }
 
         return $data;
+    }
+
+    private function loadBugsnag($bugsnag_api_key)
+    {
+        $this->bugsnag_client = null;
+
+        if (class_exists('Bugsnag_Client')) {
+            $this->bugsnag_client = new Bugsnag_Client($bugsnag_api_key);
+        }
+    }
+
+    private function bugsnagNotifyException($e)
+    {
+        if($this->bugsnag_client) {
+            $this->bugsnag_client->notifyException($e);
+        }
+    }
+
+    private function bugsnagNotifyError($error_type, $error_message)
+    {
+        if($this->bugsnag_client) {
+            $this->bugsnag_client->notifyError($error_type, $error_message);
+        }
     }
 }
