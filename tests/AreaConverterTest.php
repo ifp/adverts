8d347674e0fdc52dd512f42571e48761fc4ad781
Andy Knight <andzandz@users.noreply.github.com>
2016-10-11 15:50:38 +0000

    Added test for 0 area being formatted correctly

--- tests/AreaConverterTest.php
+++ tests/AreaConverterTest.php
@@ -327,6 +327,23 @@ class AreaConverterTest extends PHPUnit_Framework_TestCase
             $area_converter->from(0.4047, 'ha')->to('ac')->formattedValueAndUnit());
     }
 
+    public function testFormattedValueZero()
+    {
+        $area_converter = new AreaConverter();
+
+        $this->assertEquals('0 m²',
+            $area_converter->from(0, 'm²')->formattedValueAndUnit());
+
+        $this->assertEquals('0 m²',
+            $area_converter->from(0, 'm²')->to('m²')->formattedValueAndUnit());
+
+        $this->assertEquals('0 ha',
+            $area_converter->from(0, 'm²')->to('ha')->formattedValueAndUnit());
+
+        $this->assertEquals('0 acres',
+            $area_converter->from(0, 'ha')->to('ac')->formattedValueAndUnit());
+    }
+
     public function testFormattedValueWhenNotConvertedYet()
     {
         $area_converter = new AreaConverter();
@@ -359,6 +376,8 @@ class AreaConverterTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('0.5 acres (2024 m²)',
             $area_converter->from(2024, 'm²')->to('ac')->formattedValueAndUnit(['show_additional_conversion_under_1' => true]));
     }
+
+
 
     // A slightly contrived test, but this should be tested somehow just in case
     public function testInstanceReturnedByselectConversionUnitMethod()
