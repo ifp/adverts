1745ddb61660a1a61fb7fadf65ff70bbacf95610
Andy Knight <andzandz@users.noreply.github.com>
2018-03-12 12:50:28 +0000

    SearchSorter: added methods for sort by saved_at asc and desc

--- tests/Sales/SearchSorterTest.php
+++ tests/Sales/SearchSorterTest.php
@@ -131,6 +131,70 @@ class SearchSorterTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('/sale-advert-search?minimum_price=100000&maximum_price=2000000&minimum_bedrooms=1&maximum_bedrooms=6&minimum_land_size=300&maximum_land_size=400000&regions=aquitaine,centre&departments=dordogne,haute-loire&keywords_any=swimming+pool&sort_by=land_size&sort_direction=desc', $subject->landSizeDescendingUrl());
     }
 
+    public function testSorterCanCreateASavedAtDescendingUrlForTheCurrentSearch()
+    {
+        $search_criteria = [
+            "start_page" => 1,
+            "page_size" => 15,
+            "minimum_price" => 100000,
+            "maximum_price" => 2000000,
+            "minimum_bedrooms" => 1,
+            "maximum_bedrooms" => 6,
+            "minimum_land_size" => 300,
+            "maximum_land_size" => 400000,
+            "sort_by" => "price",
+            "sort_direction" => "asc",
+            "regions" => [
+                "aquitaine",
+                "centre",
+            ],
+            "departments" => [
+                "dordogne",
+                "haute-loire",
+            ],
+            "keywords_any" => "swimming pool",
+        ];
+
+        $base_url = '/sale-advert-search';
+
+        $subject = new SearchSorter($base_url, $search_criteria);
+
+        $this->assertEquals('/sale-advert-search?minimum_price=100000&maximum_price=2000000&minimum_bedrooms=1&maximum_bedrooms=6&minimum_land_size=300&maximum_land_size=400000&regions=aquitaine,centre&departments=dordogne,haute-loire&keywords_any=swimming+pool&sort_by=saved_at&sort_direction=desc',
+            $subject->savedAtDescendingUrl());
+    }
+
+    public function testSorterCanCreateASavedAtAscendingUrlForTheCurrentSearch()
+    {
+        $search_criteria = [
+            "start_page" => 1,
+            "page_size" => 15,
+            "minimum_price" => 100000,
+            "maximum_price" => 2000000,
+            "minimum_bedrooms" => 1,
+            "maximum_bedrooms" => 6,
+            "minimum_land_size" => 300,
+            "maximum_land_size" => 400000,
+            "sort_by" => "price",
+            "sort_direction" => "asc",
+            "regions" => [
+                "aquitaine",
+                "centre",
+            ],
+            "departments" => [
+                "dordogne",
+                "haute-loire",
+            ],
+            "keywords_any" => "swimming pool",
+        ];
+
+        $base_url = '/sale-advert-search';
+
+        $subject = new SearchSorter($base_url, $search_criteria);
+
+        $this->assertEquals('/sale-advert-search?minimum_price=100000&maximum_price=2000000&minimum_bedrooms=1&maximum_bedrooms=6&minimum_land_size=300&maximum_land_size=400000&regions=aquitaine,centre&departments=dordogne,haute-loire&keywords_any=swimming+pool&sort_by=saved_at&sort_direction=asc',
+            $subject->savedAtAscendingUrl());
+    }
+
     public function testSorterCanIdentifyPriceAscendingSortIsCurrentlySelected()
     {
         $search_criteria = [
@@ -253,5 +317,67 @@ class SearchSorterTest extends PHPUnit_Framework_TestCase
         $subject = new SearchSorter($base_url, $search_criteria);
 
         $this->assertEquals('land_size_desc', $subject->currentSort());
+    }
+
+    public function testSorterCanIdentifySavedAtDescendingSortIsCurrentlySelected()
+    {
+        $search_criteria = [
+            "start_page" => 1,
+            "page_size" => 15,
+            "minimum_price" => 100000,
+            "maximum_price" => 2000000,
+            "minimum_bedrooms" => 1,
+            "maximum_bedrooms" => 6,
+            "minimum_land_size" => 300,
+            "maximum_land_size" => 400000,
+            "sort_by" => "saved_at",
+            "sort_direction" => "desc",
+            "regions" => [
+                "aquitaine",
+                "centre",
+            ],
+            "departments" => [
+                "dordogne",
+                "haute-loire",
+            ],
+            "keywords_any" => "swimming pool",
+        ];
+
+        $base_url = '/sale-advert-search';
+
+        $subject = new SearchSorter($base_url, $search_criteria);
+
+        $this->assertEquals('saved_at_desc', $subject->currentSort());
+    }
+
+    public function testSorterCanIdentifySavedAtAscendingSortIsCurrentlySelected()
+    {
+        $search_criteria = [
+            "start_page" => 1,
+            "page_size" => 15,
+            "minimum_price" => 100000,
+            "maximum_price" => 2000000,
+            "minimum_bedrooms" => 1,
+            "maximum_bedrooms" => 6,
+            "minimum_land_size" => 300,
+            "maximum_land_size" => 400000,
+            "sort_by" => "saved_at",
+            "sort_direction" => "asc",
+            "regions" => [
+                "aquitaine",
+                "centre",
+            ],
+            "departments" => [
+                "dordogne",
+                "haute-loire",
+            ],
+            "keywords_any" => "swimming pool",
+        ];
+
+        $base_url = '/sale-advert-search';
+
+        $subject = new SearchSorter($base_url, $search_criteria);
+
+        $this->assertEquals('saved_at_asc', $subject->currentSort());
     }
 }
